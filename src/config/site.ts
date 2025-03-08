d596b1edd7b87f83b743f044463aa046361705f2
Chaoying <chaunceywe@gmail.com>
2025-03-08 11:22:16 +0800

    Add keywords

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,11 +2,27 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Freenote",
-  url: "freenote.celerforge.com",
-  keywords: ["freenote"],
+  url: "freenote.app",
+  keywords: [
+    "AI journal app",
+    "Markdown support",
+    "note-taking application",
+    "AI-assisted note search",
+    "AI-assisted note summarization",
+    "creativity management",
+    "local data storage",
+    "data privacy",
+    "data security",
+    "offline journal app",
+    "secure note-taking",
+    "AI-driven journaling",
+    "personal knowledge base",
+    "digital diary with AI",
+    "private journaling app",
+  ],
   title: "Freenote - Write Freely, Note Casually",
   description:
-    "Freenote is a note-taking app that lets you capture your thoughts freely, whenever and however you want.",
+    "Freenote is an open-source AI journal app with Markdown support. You can freely record your notes, and with AI assistance, easily search and summarize past entries, enhancing creativity capture and management. All data is stored locally on your device, ensuring privacy and security.",
   twitter: "https://twitter.com/chaoying972",
-  domain: "https://freenote.celerforge.com",
+  domain: "https://freenote.app",
 };
