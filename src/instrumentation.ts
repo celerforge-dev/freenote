24ca82fc0a023fbde4d90429b7659af8cd9e6b3f
Chaoying <chaunceywe@gmail.com>
2025-03-08 19:22:57 +0800

    Disable otel

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,9 +1,9 @@
-import { registerOTel } from "@vercel/otel";
-import { LangfuseExporter } from "langfuse-vercel";
+// import { registerOTel } from "@vercel/otel";
+// import { LangfuseExporter } from "langfuse-vercel";
 
 export function register() {
-  registerOTel({
-    serviceName: "langfuse-vercel-ai-nextjs-example",
-    traceExporter: new LangfuseExporter(),
-  });
+  // registerOTel({
+  //   serviceName: "freenote",
+  //   traceExporter: new LangfuseExporter(),
+  // });
 }
